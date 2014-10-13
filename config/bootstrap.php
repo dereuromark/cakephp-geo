83c98bf999c6bc2168bc7653652926d502960891
cewi <cewi@users.noreply.github.com>
2014-10-13 22:14:17 +0200

    Update bootstrap.php
    changed Log configuration

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,11 +1,9 @@
 <?php
 
-use Cake\Core\Configure;
-use Cake\Core\Configure\Engine\PhpConfig;
 use Cake\Log\Log;
 
-Log::config('scope_test', [
-	'engine' => Configure::read('App.namespace'),
-	'types' => array('debug'),
-	'scopes' => array('geocode'),
+Log::config('geo', [
+	'engine' => 'File',
+	'types' => ['debug'],
+	'scopes' => ['geocode'],
 ]);
