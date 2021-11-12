12badddaeb9bc6875bfc5cf88bb62b72b1b966a6
mscherer <mark.scherer@spryker.com>
2021-11-12 09:40:57 +0100

    Fix up routes file.

--- config/routes.php
+++ config/routes.php
@@ -1,9 +1,12 @@
 <?php
+/**
+ * @var \Cake\Routing\RouteBuilder $routes
+ */
 
 use Cake\Routing\RouteBuilder;
 use Cake\Routing\Router;
 
-Router::prefix('Admin', function (RouteBuilder $routes) {
+$routes->prefix('Admin', function (RouteBuilder $routes) {
 	$routes->plugin('Geo', function (RouteBuilder $routes) {
 		$routes->connect('/', ['controller' => 'Geo', 'action' => 'index']);
 
