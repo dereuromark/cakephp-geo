c7bbc3a4254d00b2c9bdeedc648f28c2fd8019a6
VigiePirate <42588241+VigiePirate@users.noreply.github.com>
2022-07-14 17:53:04 +0200

    Remove routing deprecations
    `Router::prefix()` is deprecated in Cakephp 4.4. Code modified to use the non-static method `RouteBuilder::prefix()`

--- config/routes.php
+++ config/routes.php
@@ -1,9 +1,8 @@
 <?php
 
 use Cake\Routing\RouteBuilder;
-use Cake\Routing\Router;
 
-Router::prefix('Admin', function (RouteBuilder $routes) {
+$routes->prefix('Admin', function (RouteBuilder $routes) {
 	$routes->plugin('Geo', function (RouteBuilder $routes) {
 		$routes->connect('/', ['controller' => 'Geo', 'action' => 'index']);
 
